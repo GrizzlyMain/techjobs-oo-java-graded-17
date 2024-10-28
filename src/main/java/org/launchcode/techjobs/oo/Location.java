ca2d2e5746406eef39d4dd56a24e1759d0a2b3ca
GrizzlyMain <ryan.thom.dalton@gmail.com>
2024-10-27 21:10:53 -0500

    Refactored DRY Location

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -4,18 +4,22 @@ import java.util.Objects;
 
 public class Location extends JobField{
 
+    public Location(String value) {
+        super(value);
+    }
+
 //    private int id;
 //    private static int nextId = 1;
 //    private String value;
 
-    public Location() {
-        id = nextId;
-        nextId++;
-    }
-    public Location (String value){
-        this();
-        this.value = value;
-    }
+//    public Location() {
+//        id = nextId;
+//        nextId++;
+//    }
+//    public Location (String value){
+//        this();
+//        this.value = value;
+//    }
     // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
     //  constructor should also call the empty constructor in order to initialize the 'id' field.
 
@@ -27,13 +31,13 @@ public class Location extends JobField{
 //        return value;
 //    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Location)) return false;
-        Location location = (Location) o;
-        return getId() == location.getId();
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof Location)) return false;
+//        Location location = (Location) o;
+//        return getId() == location.getId();
+//    }
 
 //    @Override
 //    public int hashCode() {
